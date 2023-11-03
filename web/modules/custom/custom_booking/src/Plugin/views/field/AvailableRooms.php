01081c5fa13ae8d8aee0895b4324e85ad3049cf7
omkar-3328 <omkar.yewale@rishabhsoft.com>
2023-11-03 12:08:37 +0530

    rooms avaiblity features for both form and view  level and smtp installed

---
+++ web/modules/custom/custom_booking/src/Plugin/views/field/AvailableRooms.php
@@ -0,0 +1,157 @@
+<?php
+
+namespace Drupal\custom_booking\Plugin\views\field;
+
+use Drupal\views\Plugin\views\display\DisplayPluginBase;
+use Drupal\views\Plugin\views\field\FieldPluginBase;
+use Drupal\views\ResultRow;
+use Drupal\views\ViewExecutable;
+use Drupal\webform\Entity\WebformSubmission;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Defines a custom Views field for available rooms.
+ *
+ * @ingroup views_field_handlers
+ *
+ * @ViewsField("available_rooms")
+ */
+class AvailableRooms extends FieldPluginBase {
+
+
+  /**
+   * The current display.
+   *
+   * @var string
+   *   The current display of the view.
+   */
+  protected $currentDisplay;
+
+  /**
+   * The request object.
+   *
+   * @var \Symfony\Component\HttpFoundation\Request
+   */
+  protected $request;
+
+  /**
+   * The common services.
+   *
+   * @var \Drupal\custom_booking\Services\CommonServices
+   */
+  protected $commonServices;
+
+  /**
+   * Constructs a new AvailableRooms object.
+   *
+   * @param array $configuration
+   *   The configuration.
+   * @param string $plugin_id
+   *   The plugin_id.
+   * @param mixed $plugin_definition
+   *   The plugin definition.
+   * @param \Symfony\Component\HttpFoundation\Request $request
+   *   The request object.
+   * @param \Drupal\custom_booking\Services\CommonServices $commonServices
+   *   The common services.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, $request, $commonServices) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->currentDisplay = $configuration['view']->current_display;
+    $this->request = $request;
+    $this->commonServices = $commonServices;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('request_stack')->getCurrentRequest(),
+      $container->get('common_services')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function init(ViewExecutable $view, DisplayPluginBase $display, array &$options = NULL) {
+    parent::init($view, $display, $options);
+    $this->currentDisplay = $view->current_display;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function usesGroupBy() {
+    return FALSE;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function query() {
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function defineOptions() {
+    $options = parent::defineOptions();
+    // First check whether the field should be hidden,
+    // if the value(hide_alter_empty = TRUE)
+    // the rewrite is empty (hide_alter_empty = FALSE).
+    $options['hide_alter_empty'] = ['default' => FALSE];
+    return $options;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function render(ResultRow $values) {
+    $node = $values->_entity;
+    $nid = $node->get('nid')->getValue()[0]['value'];
+    $capacity = $node->get('field_room_capacity')->getValue()[0]['value'];
+    $guesthouseId = $node->get('field_guesthouse')->getValue()[0]['target_id'];
+    $startDate = $this->request->query->get('start_date');
+    $endDate = $this->request->query->get('end_date');
+    $class = 'occupancy-green';
+    $output = '';
+    if ($startDate != '' && $endDate != '') {
+      // Get booking Ids.
+      $bookedRoomNids = $this->commonServices->getBookedNids($startDate, $endDate, $nid, $guesthouseId);
+      // Checking for single occupancy booking.
+      if (count($bookedRoomNids) == 1) {
+        $submission = WebformSubmission::load(reset($bookedRoomNids));
+        if ($submission) {
+          $occupancy = $submission->getElementData('room_occupancy');
+          if ($occupancy == 'single') {
+            $class = 'occupancy-orange';
+            $output = 'Booked for single occupancy';
+          }
+          else {
+            $available = $capacity - count($bookedRoomNids);
+            $output = $available > 0 ? $available . ' Slot' . ($available > 1 ? 's' : '') . ' available' : 'Fully Booked';
+            $class = $available > 0 ? 'occupancy-green' : 'occupancy-red';
+          }
+        }
+      }
+      else {
+        $available = $capacity - count($bookedRoomNids);
+        $output = $available > 0 ? $available . ' Slot' . ($available > 1 ? 's' : '') . ' available' : 'Fully Booked';
+        $class = $available > 0 ? 'occupancy-green' : 'occupancy-red';
+      }
+    }
+    else {
+      $output = 'Available';
+    }
+
+    return [
+      '#markup' => '<div class="' . $class . '">' . $output . '</div>',
+    ];
+  }
+
+}
--- web/modules/custom/custom_booking/src/Services/CommonServices.php
+++ web/modules/custom/custom_booking/src/Services/CommonServices.php
@@ -3,6 +3,7 @@
 namespace Drupal\custom_booking\Services;
 
 use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\webform\Entity\WebformSubmission;
 
 /**
  * Provides a common service to fetch node titles based on content type.
@@ -66,6 +67,88 @@ class CommonServices {
     }
 
     return $totalCost;
+  }
+
+  /**
+   * Get room list options value.
+   */
+  public function getRoomListOptions($guesthouseId, $startDate, $endDate) {
+    $nodeStorage = $this->entityTypeManager->getStorage('node');
+
+    $query = $nodeStorage->getQuery()
+      ->condition('type', 'rooms')
+      ->condition('field_guesthouse', $guesthouseId)
+      ->accessCheck(TRUE);
+    $subquery = \Drupal::service('webform_query');
+    $subquery->addCondition('guesthouse', $guesthouseId);
+    $subquery->addCondition('start_date', $endDate, '<=');
+    $subquery->addCondition('end_date', $startDate, '>=');
+    $bookedRoomNids = $subquery->processQuery()->fetchCol();
+    $submissionId = $this->getSubmissionId();
+    // For edit form confition.
+    if (!empty($submissionId) || $submissionId != '') {
+      if (in_array($submissionId, $bookedRoomNids)) {
+        $bookedRoomNids = array_diff($bookedRoomNids, [$submissionId]);
+      }
+    }
+    foreach ($bookedRoomNids as $bookedRoomNid) {
+      $submission = WebformSubmission::load($bookedRoomNid);
+      if ($submission) {
+        $occupancy = $submission->getElementData('room_occupancy');
+        $room = $submission->getElementData('assign_room');
+        if ($occupancy == 'single') {
+          $arr[] = $room;
+        }
+        else {
+          // Load room capacity.
+          $roomCapacity = $this->getRoomCapacity($room);
+          $bookingIds = $this->getBookedNids($startDate, $endDate, $room, $guesthouseId);
+          if (count($bookingIds) >= $roomCapacity) {
+            $arr[] = $room;
+          }
+        }
+      }
+    }
+    if (!empty($arr)) {
+      $query->condition('nid', $arr, 'NOT IN');
+    }
+    $roomNids = $query->execute();
+    if (!empty($roomNids)) {
+      // $options = ['selected' => '- Select a value -'];
+      $room_storage = $this->entityTypeManager->getStorage('node');
+      $options = [];
+      foreach ($roomNids as $nid) {
+        $room = $room_storage->load($nid);
+        if ($room) {
+          $options[$room->id()] = $room->label();
+        }
+      }
+    }
+    return $options;
+  }
+
+  /**
+   * Get room Capacity value.
+   */
+  public function getRoomCapacity($roomId) {
+    $node = $this->entityTypeManager->getStorage('node')->load($roomId);
+    $capacity = $node->field_room_capacity->value;
+
+    return $capacity;
+  }
+
+  /**
+   * Get webform submission Id.
+   */
+  public function getSubmissionId() {
+    // Get the submission ID from the route parameters.
+    $route_match = \Drupal::routeMatch();
+    $submissionId = $route_match->getParameter('webform_submission');
+    if ($submissionId instanceof WebformSubmission) {
+      $submissionId = $submissionId->id();
+    }
+
+    return $submissionId;
   }
 
 }
