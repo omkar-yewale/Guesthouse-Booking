a624c14a73feeb52e269ea34aa2a6716d3e42449
Arti Pattewar <Arti.Pattewar@rishabhsoft.com>
2023-12-19 13:51:04 +0530

    edit-page-js-change

--- web/modules/custom/custom_booking/assets/js/webform_settings.js
+++ web/modules/custom/custom_booking/assets/js/webform_settings.js
@@ -1,21 +1,33 @@
 (function ($) {
     $(document).ready(function () {
-        if(('#edit-employee-details #edit-employee-status').length) {
+        if(('#edit-employee-details--2 #edit-employee-status').length) {
             //hides employee name and email field when the default selected option is old - existing employee
-            var currentValue = $("#edit-employee-details #edit-employee-status option:selected").val();
-            if(currentValue == 'old') {
+           var currentValue = $("#edit-employee-details--2 #edit-employee-status option:selected").val();
+           if(currentValue == 'old') {
                 $('.form-item-employee-name').addClass('field-display-none');
                 $('.form-item-employee-email').addClass('field-display-none');
+                $('.js-form-item-employee-email').addClass('field-display-none');
+                $('.js-form-item-employee-name').addClass('field-display-none');
+            }
+            else {
+                $('.form-item-employee-name').removeClass('field-display-none');
+                $('.form-item-employee-email').removeClass('field-display-none');
+                $('.js-form-item-employee-email').removeClass('field-display-none');
+                $('.js-form-item-employee-name').removeClass('field-display-none');
             }
             $('#edit-employee-status').on('change', function () {
                 var selectedValue = $(this).val();
                 if(selectedValue == 'old') {
                     $('.form-item-employee-name').addClass('field-display-none');
                     $('.form-item-employee-email').addClass('field-display-none');
+                    $('.js-form-item-employee-email').addClass('field-display-none');
+                    $('.js-form-item-employee-name').addClass('field-display-none');
                 }
                 else {
                     $('.form-item-employee-name').removeClass('field-display-none');
                     $('.form-item-employee-email').removeClass('field-display-none');
+                    $('.js-form-item-employee-email').removeClass('field-display-none');
+                    $('.js-form-item-employee-name').removeClass('field-display-none');
                 }
             });
         }
